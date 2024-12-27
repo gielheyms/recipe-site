d75eab89e13a4957cea06cb71471050e1f6a5852
gielheyms <gielheyms@gmail.com>
2024-12-27 19:43:12 +0100

    Intial commit

---
+++ randomrecipe.js
@@ -0,0 +1,4 @@
+document.getElementById('random-recipe').addEventListener('click', () => {
+  // Placeholder for random recipe fetch (from backend later)
+  alert('Random recipe coming soon!');
+});
